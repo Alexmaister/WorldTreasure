4ad89f04d6944893e2a523293a2fbc45d80aedfe
Unknown <aortizurita@gmail.com>
2018-01-26 11:58:35 +0100

    parece ir bien

--- Example1/core/src/mundo/Mundo.java
+++ Example1/core/src/mundo/Mundo.java
@@ -17,8 +17,15 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.attributes.FloatAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Frustum;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import org.ietf.jgss.GSSException;
 
 import componentes.ModeloComponente;
 import componentes.SistemaRenderizado;
@@ -31,6 +38,7 @@ public class Mundo {
 
     //Camara ortografica para el juego en perspectiva de tercera persona
     private OrthographicCamera camara;
+
     //Entorno el cual contendra las luces de entorno :)
     private Environment entorno;
 
@@ -52,7 +60,14 @@ public class Mundo {
                 new Entity().add(
                         new ModeloComponente(
                                 new ModelBuilder()
-                                        .createBox(20f,20f,20f,new Material(ColorAttribute.createDiffuse(Color.BLUE)),VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal),0f,0f,0f)));
+                                        .createBox(50f,50f,50f,new Material(ColorAttribute.createDiffuse(Color.RED)),VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal),0f,50f,0f)));
+
+        motor.addEntity(
+                new Entity().add(
+                        new ModeloComponente(
+                                new ModelBuilder()
+                                        .createBox(20000f,1f,20000f,new Material(ColorAttribute.createDiffuse(Color.BLUE)),VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal),0f,0f,0f)));
+
 
         motor.addSystem(new SistemaRenderizado(renderizador,entorno));
     }
@@ -60,16 +75,20 @@ public class Mundo {
 
     private void iniciarCamara(){
 
-        camara=new OrthographicCamera(30,30*(Gdx.graphics.getHeight()/Gdx.graphics.getWidth()));
+        camara=new OrthographicCamera();
 
-        camara.position.set(camara.viewportWidth/2f, camara.viewportHeight/2f, 0);
+        camara.position.set(40f ,200f, 40f);
+        camara.up.set(0,1,0);
         camara.lookAt(0f,0f,0f);
-//        camara.rotate(Vector3.X,20);
-        //camara.near=1f;
-        //camara.far=1000f;
-        //camara.setToOrtho(true);
-        //camara.zoom=1;
+
+        //camara.rotate(Vector3.,10);
+        camara.near=0.3f;
+        camara.far=10000f;
+        //camara.setToOrtho(true, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        //camara.zoom= (float) 2;
         camara.update();
+
+
     }
 
     private void iniciarEntorno(){
@@ -111,9 +130,26 @@ public class Mundo {
        // camara.update();
 
         renderizador.begin(camara);
-       //ModeloComponente entity = motor.getEntities().first().getComponent(ModeloComponente.class);
-      // entity.instancia.transform.setToTranslation(50f,10f,100f);
 
+       //
+        if(Gdx.input.isTouched())
+        if(Gdx.input.getX()>0){
+
+            ModeloComponente entity = motor.getEntities().first().getComponent(ModeloComponente.class);
+            Vector3 posicion=new Vector3();
+
+            entity.instancia.transform.getTranslation(posicion);
+            posicion.x+=10;
+            posicion.z-=10;
+
+            //posicion.z-=10;
+            camara.position.x+=10;
+            camara.position.z-=10;
+            camara.update();
+            entity.instancia.transform.setTranslation(posicion.x,posicion.y,posicion.z);
+
+        }
+        motor.getEntities().first().getComponent(ModeloComponente.class).instancia.transform.rotate(Vector3.Y,10);
         motor.update(delta);
         renderizador.end();
     }
